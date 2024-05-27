8d8370315ae0873deb44a7a68bee32c46fdc7551
CutieMarisol00 <aquinomarisol93@gmail.com>
2024-05-27 09:15:32 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const btnLike1 = document.getElementById("btnLike1")
+const countLike1 = document.getElementById("countLike1")
+
+function clickLike1(){
+  let totalLikes = parseInt(countLike1.value) + 1
+  countLike1.textContent = totalLikes.toString()
+}
+btnLike1.addEventListener("click",clickLike1)
+
+function clickLike2(){
+  let totalLikes = parseInt(countLike2.value) + 1
+  countLike2.textContent = totalLikes.toString()
+}
+btnLike2.addEventListener("click",clickLike2)
