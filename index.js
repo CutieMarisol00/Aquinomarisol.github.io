11b1478ef91057ed86ab8a152597c2368569f970
CutieMarisol00 <aquinomarisol93@gmail.com>
2024-06-08 17:40:13 +0800

    Update index.js

--- index.js
+++ index.js
@@ -12,3 +12,18 @@ function clickLike2(){
   countLike2.textContent = totalLikes.toString()
 }
 btnLike2.addEventListener("click",clickLike2)
+
+
+const submit = document.getElementById("submit")
+
+const comment = document.getElementById("comment")
+
+const commentbox = document.getElementById("commentbox")
+
+function submitComment(){
+
+commentbox.textContent += comment.value.toString() + "\n"
+
+}
+
+submit.addEventListener("click", submitComment)
